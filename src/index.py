529392d6e6835ed34dd59d461a612ed3f1ff6099
James Robins <knockknockitsjimmy@outlook.com>
2021-09-10 20:33:12 +1000

    added routes to forms

--- src/index.py
+++ src/index.py
@@ -1,14 +1,27 @@
 from flask import Flask, request, render_template, Blueprint, render_template, redirect, url_for, request, flash
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
 
-@app.route("/create")
+@app.route("/create", methods=['GET', 'POST'])
 def create():
-    return render_template("create.html")
+    if request.method == 'GET':
+        return render_template("create.html")
+
+@app.route("/join", methods=['GET', 'POST'])
+def join():
+    if request.method == 'GET':
+        return render_template("join.html")
+
+@app.route("/play", methods=['GET', 'POST'])
+def play():
+    if request.method == 'GET':
+        return render_template("play.html")
 
 if __name__ == '__main__':
     app.run()
