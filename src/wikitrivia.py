dd28da0b14008fb09e4d6c4d39cf52eafdc16ae6
Bobby Melhem <bobbyjacksonmelhem@gmail.com>
2021-09-11 14:32:56 +1000

    Similarity Test added

--- src/wikitrivia.py
+++ src/wikitrivia.py
@@ -5,6 +5,7 @@ import wikipedia
 import random
 import re
 import nltk
+from difflib import SequenceMatcher
 
 #Download natural language packages
 path = './nltk_modules'
@@ -18,6 +19,8 @@ nltk.download('words', download_dir=path, quiet=True)
 
 BLANK_SPACE = "______"
 
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 def strip_brackets(text):
     return re.sub('\(.*\)', "", text)
@@ -34,22 +37,45 @@ def fetch_named_entities(text):
 
 
 def remove_subject(subject, text):
-    text = strip_brackets(text)
+    remove_list = []
+    questions = strip_brackets(text)
+    subject_split = subject.split(" ")
+    remove_list.extend(subject_split)
 
     subject_labels = fetch_named_entities(subject)
     text_labels = fetch_named_entities(text)
 
+    #Store all synonyms of subject
+    subject_synsets = []
+    for label in subject_labels:
+        synset = nltk.corpus.wordnet.synsets(label[1])
+        for syn in synset:
+            subject_synsets.append(syn)
+
     for item in text_labels:
-        if item[0] == text_labels[0][0]: #TODO: Generalise to all types
-            print(item)
-            #if nltk.corpus.wordnet.synsets(item[1]):
-                #text = text.replace(item[1], BLANK_SPACE)
-                    
-    subject_split = subject.split(" ")
-    for token in subject_split:
-        text = text.replace(token, BLANK_SPACE)
-        
-    return text
+        for token in subject_split:
+            if similar(item[1], token) > 0.75:
+                print(item[1], " : ", token, similar(item[1], token))
+                remove_list.append(item[1])
+
+        if item[0] == text_labels[0][0]:
+            #print(item)
+
+            #Catch all synonyms of the subject
+            synset = nltk.corpus.wordnet.synsets(item[1])
+            if synset:
+                for syn in synset:
+                    if syn in subject_synsets:
+                        #print(syn, "!!!!!!")
+                        remove_list.append(item[1])
+
+    #Attempt to remove big words first
+    remove_list.sort(key=len, reverse=True)
+    for item in remove_list:
+        questions = questions.replace(item, BLANK_SPACE)
+
+                        
+    return questions
 
 
 def generate_question():
@@ -72,7 +98,7 @@ def generate_question():
     pageLoaded = False
     while pageLoaded == False:
         try:
-            random.seed(50)
+            random.seed(69)
             random_page = random.choice(links)
             question_page = wikipedia.page(title=random_page,auto_suggest=False)
             pageLoaded = True
