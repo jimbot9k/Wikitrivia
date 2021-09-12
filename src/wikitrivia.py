20f7854e181a3a32a770f801d0f799c6103e4825
James Robins <knockknockitsjimmy@outlook.com>
2021-09-12 13:50:10 +1000

    lol

--- src/wikitrivia.py
+++ src/wikitrivia.py
@@ -310,7 +310,7 @@ def generate_question(question_set="top annual"):
 
 #(answer, summary) = generate_question("random")
 #(answer, summary) = generate_question("top annual")
-question = generate_question("grossing animes")
-print(question.question, " : ", question.answer, ' or ', question.falseAnswers[0], ' or ', question.falseAnswers[1], ' or ', question.falseAnswers[2])
+#question = generate_question("grossing animes")
+#print(question.question, " : ", question.answer, ' or ', question.falseAnswers[0], ' or ', question.falseAnswers[1], ' or ', question.falseAnswers[2])
 
 #Maybe add some movies or actors etc pages so you can have specific categories.
