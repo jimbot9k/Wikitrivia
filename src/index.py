ff6e9c7e7b9e9c8d7c0ddaf021f95747ce9f26c3
Yash Talekar <yashctalekar@gmail.com>
2021-09-12 13:32:55 +1000

    Some visual changes.

--- src/index.py
+++ src/index.py
@@ -63,11 +63,6 @@ def create_game(json):
         msg["answer{i}".format(i=answer)] = games[roomID].get_answers()[answer]
     emit('updateQuestion', JSON.dumps(msg), to=roomID)
 
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data)
-
-
 @socketio.on("joinGame")
 def join_game(json):
     roomID = json["roomID"]
